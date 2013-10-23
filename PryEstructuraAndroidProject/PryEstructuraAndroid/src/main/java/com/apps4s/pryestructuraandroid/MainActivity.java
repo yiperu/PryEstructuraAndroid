3f5ddb9c8523ec6f563adf64273661b82b5c58eb
Henry AT <yiperu@gmail.com>
2013-10-23 17:57:09 -0300

    se configuro el pry para mostrar el texto programaticamente, ademas que se adiciono un xml para lso IDs

--- PryEstructuraAndroidProject/PryEstructuraAndroid/src/main/java/com/apps4s/pryestructuraandroid/MainActivity.java
+++ PryEstructuraAndroidProject/PryEstructuraAndroid/src/main/java/com/apps4s/pryestructuraandroid/MainActivity.java
@@ -3,13 +3,24 @@ package com.apps4s.pryestructuraandroid;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+    TextView tv1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv1 = (TextView)findViewById(R.id.cad1);
+        boolean condicion = false;
+        if (condicion){
+            tv1.setText("Me llamo Henry");
+        } else {
+            tv1.setText("Mi apellido es Ambicho");
+        }
+
     }
 
 
